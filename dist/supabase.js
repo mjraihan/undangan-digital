43dc17abadcd24c3471b52a8179e069a28fc3e2a
mjraihan <mjraihan000@gmail.com>
2026-06-23 11:00:33 +0700

    init

---
+++ dist/supabase.js
@@ -0,0 +1,7 @@
+const supabaseUrl = "https://mtvbgfrcjguekpbbrjku.supabase.co";
+const supabaseKey = "sb_publishable_cundlp8sAgtABE9ORvsjpQ_kuqm-r0-";
+
+window.supabaseClient = window.supabase.createClient(
+    supabaseUrl,
+    supabaseKey
+);
